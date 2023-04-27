29e3a501fe2d11fb2d5df7b7bfdedcfbd53a647a
Jan Škvařil <kondr4@seznam.cz>
2023-04-27 15:53:52 +0200

    Feature: Added finished page

---
+++ AeroBarista/ViewModels/FinishedPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using AeroBarista.Attributes;
+using AeroBarista.Services.Interfaces;
+using AeroBarista.ViewModels.Base;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+
+
+namespace AeroBarista.ViewModels
+{
+    [ExportTransient]
+    public partial class FinishedPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private FileResult? takenImage;
+        ICameraService cameraService;
+        INativeShareService shareService;
+
+
+        public FinishedPageViewModel(INavigationService navigationService, ICameraService cameraService, INativeShareService shareService) : base(navigationService)
+        {
+            this.cameraService = cameraService;
+            this.shareService = shareService;
+        }
+
+        [RelayCommand]
+        public void TakePhoto()
+        {
+            ObtainPhoto();
+        }
+
+        [RelayCommand]
+        public void SharePhoto()
+        {
+            ShareImage();   
+        }
+
+        public void ShareImage()
+        {
+            if (TakenImage == null) return;
+            shareService.ShareFile("Share your image", TakenImage);
+
+
+        }
+
+        public async void ObtainPhoto()
+        {
+            TakenImage = await cameraService.ObtainPhoto();
+         
+        }
+    }
+}
