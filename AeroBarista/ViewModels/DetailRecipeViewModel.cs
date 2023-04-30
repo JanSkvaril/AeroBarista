4dcd51b26fe5eb5e46897f65cb380bc0d7b2392a
Lucie Klimesova <xklimes4@fi.muni.cz>
2023-04-30 14:26:52 +0200

    Basic form of Recipe detail

--- AeroBarista/ViewModels/DetailRecipeViewModel.cs
+++ AeroBarista/ViewModels/DetailRecipeViewModel.cs
@@ -1,19 +1,67 @@
-﻿using AeroBarista.Attributes;
+﻿using AeroBarista.ApiClients.Interfaces;
+using AeroBarista.Attributes;
+using AeroBarista.Models;
 using AeroBarista.Services.Interfaces;
 using AeroBarista.ViewModels.Base;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AeroBarista.ViewModels
 {
     [ExportTransient]
     public partial class DetailRecipeViewModel : BaseViewModel
     {
-        [ObservableProperty]
-        private string name;
+        // TODO: get data from parameter, not api client!
+        IRecipeApiClient apiClient;
 
-        public DetailRecipeViewModel(INavigationService navigationService) : base(navigationService)
+        [ObservableProperty]
+        private RecipeModel? recipe;
+
+        [ObservableProperty]
+        private TimeSpan totalTime;
+
+        public DetailRecipeViewModel(INavigationService navigationService, IRecipeApiClient apiClient) : base(navigationService)
         {
-            Name = "asscc";
+            this.apiClient = apiClient;
+
+            // change here!
+            int id = 1;
+            // change end
+
+            GetData(id);
+        }
+
+        [RelayCommand]
+        public async void StartRecipe(int id)
+        {
+            await NavigationService.NavigateToAsync("//ProcessPage");
+        }
+
+        private async void GetData(int id)
+        {
+            var actual = await apiClient.GetAll();
+
+            Recipe = actual.First(r => r.Id == id);
+
+            GetTotalTime();
+        }
+
+        private void GetTotalTime()
+        {
+            TotalTime = new TimeSpan();
+
+            if (Recipe == null)
+            {
+                return;
+            }
+            
+            foreach (var step in Recipe.Steps)
+            {
+                if (step.time != null)
+                {
+                    TotalTime += (TimeSpan)step.time;
+                }
+            }
         }
     }
 }
