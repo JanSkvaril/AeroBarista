966a38c8d247da445e92da6ebb91422a55fa2afc
Jan Škvařil <kondr4@seznam.cz>
2023-05-01 11:52:19 +0200

    Feature: Search when text changed

--- AeroBarista/Views/RecipesPage.xaml.cs
+++ AeroBarista/Views/RecipesPage.xaml.cs
@@ -6,9 +6,17 @@ namespace AeroBarista.Views;
 [ExportTransient]
 public partial class RecipesPage : ContentPage
 {
+	private readonly RecipesViewModel viewModel;
+	
 	public RecipesPage(RecipesViewModel vm)
 	{
 		InitializeComponent();
-		BindingContext= vm;
+		BindingContext = vm;
+		viewModel = vm;
+	}
+
+	public void OnTextChanged(object sender, EventArgs e)
+	{
+        viewModel.Search(searchBar.Text);
 	}
 }
