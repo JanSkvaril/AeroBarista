f6890187316966f71804c545657764019a55e3e1
Jan Škvařil <kondr4@seznam.cz>
2023-05-04 23:54:46 +0200

    Improved functionality of deep links

--- AeroBarista/Platforms/Android/MainActivity.cs
+++ AeroBarista/Platforms/Android/MainActivity.cs
@@ -2,10 +2,11 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Content;
+using Microsoft.Maui.Controls;
+
 namespace AeroBarista;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-
 [IntentFilter(new[] { Intent.ActionView },
     Categories = new[]
     {
@@ -13,7 +14,7 @@ namespace AeroBarista;
         Intent.CategoryDefault,
         Intent.CategoryBrowsable
     },
-    DataScheme = "https", DataHost = "aerobarista", DataPathPrefix = "/recipe"
+    DataScheme = "https", DataHost = "aeropress", DataPathPrefix = "/recipe"
     )
 ]
 public class MainActivity : MauiAppCompatActivity
@@ -26,6 +27,18 @@ public class MainActivity : MauiAppCompatActivity
         //test
         OnNewIntent(Intent);
     }
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        var fromDeepLink = Preferences.Get("FromDeepLink", false);
+        if (fromDeepLink)
+        {
+            Preferences.Set("FromDeepLink", false);
+            Shell.Current.GoToAsync("//ProcessPage");
+        }
+    }
+
 
     protected override void OnNewIntent(Intent intent)
     {
@@ -36,8 +49,12 @@ public class MainActivity : MauiAppCompatActivity
         if (intent.Action != Intent.ActionView) return;
         if (string.IsNullOrWhiteSpace(data)) return;
 
-        if (data.Contains("/group"))
+        if (data.Contains("/recipe"))
         {
+            bool a = Shell.Current == null;
+            bool b = App.Current == null;
+            Preferences.Set("FromDeepLink", true);
+
             //do what you want 
         }
     }
