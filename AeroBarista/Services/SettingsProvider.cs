73be16c87de1ea7863b1ace5ff1a8a22791b254e
Jan Skvaril <kondr4@seznam.cz>
2023-05-06 22:17:20 +0200

    Refactor: adeded interface for settings proveder

--- AeroBarista/Services/SettingsProvider.cs
+++ AeroBarista/Services/SettingsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using AeroBarista.Attributes;
+using AeroBarista.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,13 @@ using System.Threading.Tasks;
 
 namespace AeroBarista.Services
 {
-    [ExportSingleton]
-    public class SettingsProvider
+    [ExportSingletonAs(nameof(ISettingsProvider))]
+    public class SettingsProvider : ISettingsProvider
     {
         public readonly string APP_URL = "https://aeropress/recipe";
+        public string GetAppUrl()
+        {
+            return APP_URL;
+        }
     }
 }
